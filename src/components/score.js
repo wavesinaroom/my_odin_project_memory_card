326f87bcf5631328478947204e2db0019b234ea7
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-22 12:45:29 -0500

    [Task] Add current and best hooks

--- src/components/score.js
+++ src/components/score.js
@@ -1,4 +1,8 @@
+import {useState} from "react";
+
 export default function Score(){
+ const [current, setCurrent] = useState(0); 
+ const [best, setBest] = useState(0); 
  return(
   <>
    <p>It renders!</p>
