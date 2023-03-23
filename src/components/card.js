023016e50d9a095078839226fdd197f0d4156cd5
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-22 21:57:36 -0500

    [Feature] Trigger onClick event

--- src/components/card.js
+++ src/components/card.js
@@ -2,9 +2,9 @@ export default function Card(props){
  
 
   return(
-    <>
+    <div onClick={()=>alert(`Clicked on ${props.name} card`)}>
       <p>{props.name}</p>
       <p>{props.pic}</p>
-    </>
+    </div>
   );
 }
