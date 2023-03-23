45b76438756bdd7106905d15c1ab8dbf6f7965c4
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-23 00:31:08 -0500

    [Feature] Shuffle card set

--- src/components/set.js
+++ src/components/set.js
@@ -1,11 +1,31 @@
 import {useState} from "react";
+import uniqid from "uniqid";
 import Card from "./card";
 
 export default function Set(){
-  const[cards,setCards] = useState([<Card name="John" pic="1"/>,<Card name="Al" pic="2"/>,<Card name="Paul" pic="3"/>,<Card name="James" pic="4"/>,<Card name="Lou" pic="5"/>,]);
+  const[cards,setCards] = useState([<Card key={uniqid()} name="John" pic="1"/>,
+                                    <Card key={uniqid()} name="Al" pic="2"/>,
+                                    <Card key={uniqid()} name="Paul" pic="3"/>,
+                                    <Card key={uniqid()} name="James" pic="4"/>,
+                                    <Card key={uniqid()} name="Lou" pic="5"/>,]);
 
+  function shuffle(){
+    const shuffled = Array(5);
+    let random;
+    cards.forEach(card=>{
+      while(true){
+        random = Math.floor(Math.random()*5);
+        if(!shuffled[random]){
+          shuffled[random] = card;
+          break;
+        }
+      }
+    });
+    setCards(shuffled);
+  }
   return(
     <>
+      <button onClick={()=>shuffle()}>Shuffle</button>
       {cards}
     </>
   );
