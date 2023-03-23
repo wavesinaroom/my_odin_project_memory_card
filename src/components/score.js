3de6c63e012fa92d96c2482c94a5622c76c81230
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-22 21:23:31 -0500

    [Feature] Increase current score by one

--- src/components/score.js
+++ src/components/score.js
@@ -1,11 +1,17 @@
 import {useState} from "react";
 
 export default function Score(){
- const [current, setCurrent] = useState(0); 
- const [best, setBest] = useState(0); 
- return(
-  <>
-   <p>It renders!</p>
-  </>
- ); 
+  const [current, setCurrent] = useState(0); 
+  const [best, setBest] = useState(0); 
+
+  function increase(){
+    setCurrent(current+1);
+  }
+
+  return(
+   <>
+    <button onClick={increase}>Increase</button>
+    <p>Current: {current}</p>
+   </>
+  ); 
 }
