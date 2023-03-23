64b34d2618df3288e46d2d537b18085e0d5a7fe2
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-22 21:54:02 -0500

    [Feature] Name and pic placeholders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Score from './components/score';
+import Card from './components/card';
 
 function App() {
   return (
+    <>
     <Score/>
+    <Card name="Pablo" pic="a pic"/>
+    </>
   );
 }
 
---
+++ src/components/card.js
@@ -0,0 +1,10 @@
+export default function Card(props){
+ 
+
+  return(
+    <>
+      <p>{props.name}</p>
+      <p>{props.pic}</p>
+    </>
+  );
+}
