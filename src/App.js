d9aacfeaa9aab6572000c2efea40d2bd54d27e10
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-24 11:34:03 -0500

    [Task] Draft game loop

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
 import Set from './components/set';
 import Score from './components/score';
+import {useState} from 'react';
 
 function App() {
+  const [clicked, setClicked] = useState(Array(5));
+
+  function checkClicked(id){
+    if(clicked[4]){
+      //Reset clickec - current score - best score
+    }else{
+      if(clicked.find(click => click === id)){
+        //Reset current score
+      }
+      else{
+        //Update clicked - Check best - increase current
+      }
+    }
+
+  }
   return (
     <>
       <Score/>
